e69ee147047363e431f1753f3622471de74eb9a5
Majey <kevinerick78@yahoo.com>
2022-04-07 09:04:50 -0700

    Hello World!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  * followed by a new line, to the standard output
  * Return: 1 for Success. Other number if failed
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
