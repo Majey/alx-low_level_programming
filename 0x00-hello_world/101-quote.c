93fa2cfe2ae0b5fbb78f4293b859f17effeaf2c9
Majey <kevinerick78@yahoo.com>
2022-04-07 09:16:07 -0700

    Hello world!

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful 
+ * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard output
- * Return: 0 for Success. Other number if failed
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
