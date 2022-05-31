5789b832a2aecad20c89ec0bd125e87106b97398
Majey <kevinerick78@yahoo.com>
2022-05-31 06:02:24 -0700

    Bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,19 +6,19 @@
  * @n: pointer of an unsigned long int.
  * @index: index of the bit.
  *
- * Return: 1 if it worked, -1 if it didn't.
+ * Return: 1 if success, -1 if fail.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
+	unsigned int x;
 
 	if (index > 63)
 		return (-1);
 
-	m = 1 << index;
+	x = 1 << index;
 
-	if (*n & m)
-		*n ^= m;
+	if (*n & x)
+		*n ^= x;
 
 	return (1);
 }
